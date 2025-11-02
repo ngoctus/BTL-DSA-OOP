0ed69018166c89e1e454a012159dccb259417477
ngoctus <phamngoctu00.34@gmail.com>
2025-11-03 00:10:30 +0700

    demo map lan 1 (them location, 1 so giao lo + duong)

--- traffic netwwork simulation/main.cpp
+++ traffic netwwork simulation/main.cpp
@@ -9,17 +9,30 @@ using namespace map_types;
 int main(int argc, char** argv){
 	map q1;
 	
+	//cac dia diem
 	q1.add_node(unique_ptr<node>(new location("Dinh doc lap", 11, 106.69655, 10.78251, 500, lo_type::TOURIST_ATTRACTION)));
 	q1.add_node(unique_ptr<node>(new location("Thao cam vien", 12, 106.70779, 10.78841, 1000, lo_type::TOURIST_ATTRACTION)));
 	q1.add_node(unique_ptr<node>(new location("Cong vien 23/9", 13, 106.69234, 10.76876, 1000, lo_type::TOURIST_ATTRACTION)));
 	q1.add_node(unique_ptr<node>(new location("Toa Bitexco", 14, 106.70437, 10.77172, 1500, lo_type::LANDMARK)));
-	q1.add_node(unique_ptr<node>(new junction("Giao Lo A (Auto)", 15, 106.70237, 10.76173, junction_type::UNDEFINED)));
-//	q1.add_node(unique_ptr<node>(new location("Nha tho duc ba", 5, )));
+	q1.add_node(unique_ptr<node>(new location("Nha tho duc ba", 15, 106.69900, 10.77992, 300, lo_type::TOURIST_ATTRACTION)));
+	q1.add_node(unique_ptr<node>(new location("Cho Ben Thanh", 16, 106.69805, 10.77265, 750, lo_type::TOURIST_ATTRACTION)));
+	q1.add_node(unique_ptr<node>(new location("Pho Ong Cat Gia Truyen Since 1988", 17, 106.69309, 10.76851, 25, lo_type::RESTAURANT)));
+	q1.add_node(unique_ptr<node>(new location("Nori - Modern Izakaya", 18, 106.69690, 10.77403, 50, lo_type::RESTAURANT)));
+	q1.add_node(unique_ptr<node>(new location("Trung tam bao hanh Asus", 19, 106.68918, 10.77317, 75, lo_type::OTHERS)));
 	
-	q1.add_edge_by_id("Duong Le Duan", 1112, 11, 12, 0);
-	q1.add_edge_by_id("Duong Nguyen Hue", 1512, 15, 12, 0);
-	q1.add_edge_by_id("Duong ABC", 1513, 15, 13, 0);
-	q1.add_edge_by_id("Duong ABCD", 1514, 15, 14, 1);
+	//cac giao lo
+	q1.add_node(unique_ptr<node>(new junction("GL_DDL1", 101, 106.69305, 10.77735, junction_type::UNDEFINED)));
+	q1.add_node(unique_ptr<node>(new junction("GL_DDL2", 102, 106.69500, 10.77956, junction_type::UNDEFINED)));
+	q1.add_node(unique_ptr<node>(new junction("GL_DDL3", 103, 106.69798, 10.77680, junction_type::UNDEFINED)));
+	q1.add_node(unique_ptr<node>(new junction("GL_DDL4", 104, 106.69605, 10.77475, junction_type::UNDEFINED)));
+	
+	
+	
+	
+	q1.add_edge_by_id("Duong Huyen Tran Cong Chua", 101104, 101, 104, 0);
+//	q1.add_edge_by_id("Duong Nguyen Hue", 1512, 15, 12, 0);
+//	q1.add_edge_by_id("Duong ABC", 1513, 15, 13, 0);
+//	q1.add_edge_by_id("Duong ABCD", 1514, 15, 14, 1);
 	
 //    m.add_node(unique_ptr<node>(new node("A", 1, 2, 3)));
 //	m.add_node(unique_ptr<node>(new location("B", 2, 4, 5, 20, lo_type::RESTAURANT)));
